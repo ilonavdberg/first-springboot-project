51321e020de1f87705a201ee09f8312c810f71ca
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2024-11-16 11:35:02 +0100

    boilerplate code

---
+++ src/main/java/nl/ilona/spring_postgres_test/SpringPostgresTestApplication.java
@@ -0,0 +1,13 @@
+package nl.ilona.spring_postgres_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ilona/spring_postgres_test/SpringPostgresTestApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ilona.spring_postgres_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
